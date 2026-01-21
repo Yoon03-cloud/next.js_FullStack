3a43f94d13fbe8d2f636778304aaf0baa6a1dc54
Yoon03-cloud <6631503099@lamduan.mfu.ac.th>
2026-01-21 13:15:06 +0700

    new commit

--- next-shop/app/(root)/page.tsx
+++ next-shop/app/(root)/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-xl font-bold">Welcome to the Next.js Shop</h1>
+      <h1 className="bg-red-600 text-xl font-bold">
+        Welcome to the Next.js Shop
+      </h1>
     </div>
   );
 }
