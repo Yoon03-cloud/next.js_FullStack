1b435f5744ff0b1c022cead06077c1b99e13ea66
Yoon03-cloud <6631503099@lamduan.mfu.ac.th>
2026-02-26 23:04:20 +0700

    new commit

--- next-shop/app/(root)/blog/author/page.tsx
+++ next-shop/app/(root)/blog/author/page.tsx
@@ -1,5 +1,17 @@
-function Author() {
-  return <div>This is the page for Author Information</div>;
+import prisma from "@/lib/prisma";
+
+async function Author() {
+  const authors = await prisma.user.findMany();
+  return (
+    <div>
+      <h1>Author Information</h1>
+      <ul>
+        {authors.map((author) => (
+          <li key={author.id}>{author.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Author;
--- next-shop/app/(root)/page.tsx
+++ next-shop/app/(root)/page.tsx
@@ -1,17 +1,19 @@
-import CarouselComponent from "@/component/carousel";
-import Image from "next/image";
-import Link from "next/link";
+import prisma from "@/lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const users = await prisma.user.findMany();
   return (
-    <div>
-      <h1 className="bg-red-600 text-xl font-bold">
-        Welcome to the Next.js Shop! - {new Date().toTimeString()}
+    <div className="-mt-16 flex min-h-screen flex-col items-center justify-center bg-gray-50">
+      <h1 className="mb-8 font-[family-name:var(--font-geist-sans)] text-4xl font-bold text-[#333333]">
+        Superblog
       </h1>
-      {/* <CarouselComponent /> */}
-      <Link href="/login">Already have an account? Login.</Link>
-      <Link href="/register">Don&apos;t have an account? Register.</Link>
-      <Link href="/blog">Go to Blog Page</Link>
+      <ol className="list-inside list-decimal font-[family-name:var(--font-geist-sans)]">
+        {users.map((user) => (
+          <li key={user.id} className="mb-2">
+            {user.name}
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
